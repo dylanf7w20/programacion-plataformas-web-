89b37e2c2ec0c185a4b51466a7c4b56575510d41
dylanf7w20 <dylanf7w20@gmail.com>
2022-01-27 03:26:14 -0500

    Practica_01
    Practica con el uso de HTML, CSS Y Javascript.
    Galeria de Imagenes.

---
+++ practica_01/script1.js
@@ -0,0 +1,27 @@
+var arrayImagenes = ["01.jpg" , "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg"]
+
+var imagenActual = 0;
+
+
+document.getElementById("anterior").addEventListener("click", cambiarImagen);
+document.getElementById("siguiente").addEventListener("click", cambiarImagen);
+
+
+function cambiarImagen() {
+    var boton = this.id;
+   
+    if(boton == 'anterior') {
+        imagenActual --;
+        if(imagenActual < 0) {
+            imagenActual = arrayImagenes.length -1; 
+        } 
+    } else {
+        imagenActual ++;
+        if(imagenActual == arrayImagenes.length) {
+            imagenActual = 0;
+        } 
+    }
+    var imagenAMostrar = arrayImagenes [imagenActual];
+    document.getElementById('imagen').src = 'Galeria_01/' + imagenAMostrar;
+}
+
