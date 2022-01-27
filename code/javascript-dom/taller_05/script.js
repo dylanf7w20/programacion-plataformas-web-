9a72c1edec8c7e2bd728a8e0c4be12a362148656
dylanf7w20 <dylanf7w20@gmail.com>
2022-01-27 03:23:28 -0500

    Javascript-Dom

---
+++ code/javascript-dom/taller_05/script.js
@@ -0,0 +1,14 @@
+function agregarElemento() {
+    var h1 = document.createElement('h1')
+    var texto = document.getElementById('texto')
+    var contenido = document.createTextNode( texto.value )
+    h1.appendChild( contenido )
+
+    var container = document.getElementById('container')
+    container.appendChild( h1 )
+}
+
+function eliminarElemento() {
+    var container = document.getElementById('container')
+    container.removeChild( container.lastElementChild )
+}
